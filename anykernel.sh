5350debb41ff1cbc53b12f4ba3d8330337c47b45
mitsuki-xd <102547027+tyuzu-xd@users.noreply.github.com>
2022-04-04 01:24:39 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=SixTeen Kernel by Kyvangka1610
+kernel.string=Wolf Kernel by ZenitsuXD
 do.devicecheck=1
 do.modules=1
 do.systemless=0
